9e86aacc7a340080a914875b4b002ead15a9d3ac
heroinedor <heroinedor@gmail.com>
2017-07-12 15:58:46 +0200

    Added classes and files for the "Etape 2" exercise

---
+++ src/main/java/biblio/book/Book.java
@@ -0,0 +1,34 @@
+package biblio.book;
+
+/**
+ * Created by heroinedor on 03/07/2017.
+ */
+public class Book {
+    private int idBook;
+    private String title;
+    private String content;
+
+    public int getIdBook() {
+        return idBook;
+    }
+
+    public void setIdBook(int idBook) {
+        this.idBook = idBook;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ src/test/java/biblio/book/BiblioBatchJobTest.java
@@ -0,0 +1,8 @@
+package biblio.book;
+
+/**
+ * Created by heroinedor on 12/07/2017.
+ */
+public class BiblioBatchJobTest {
+//TODO Create an end-to-end test of BiblioBatchJob
+}
