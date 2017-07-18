4ff0d5e082f803db28c4842faaf59c6cc9b23454
heroinedor <heroinedor@gmail.com>
2017-07-18 11:24:25 +0200

    Added necessary files for Etape_3 (item reader, processor, writer, etc)

---
+++ src/main/java/biblio/sentence/SentenceProcessor.java
@@ -0,0 +1,22 @@
+package biblio.sentence;
+
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.annotation.BeforeStep;
+
+/**
+ * Created by heroindor on 06/07/2017.
+ */
+public class SentenceProcessor {
+    private int bookId;
+
+    @BeforeStep
+    public void retrieveInterstepData(StepExecution stepExecution) {
+        //TODO set the bookId  with the "idBook" parameter extracted from the jobExecutioncontext
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    //TODO Implement the ItemProcessor interface  and the process method to set the bookId to the Sentence item
+}
