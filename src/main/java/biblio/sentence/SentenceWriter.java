fe62e24b3a8f19cf72409c6b4bfede1fbeae1778
heroinedor <heroinedor@gmail.com>
2017-07-19 17:01:44 +0200

    Sentence Dao, Processor and Writer changed to prepare the Tests
    # Conflicts:
    #	src/main/java/biblio/sentence/SentenceProcessor.java

--- src/main/java/biblio/sentence/SentenceWriter.java
+++ src/main/java/biblio/sentence/SentenceWriter.java
@@ -17,4 +17,8 @@ public class SentenceWriter implements ItemWriter<Sentence> {
             sentenceDao.insert(phrase);
         }
     }
+
+    public void setSentenceDao(SentenceDao sentenceDao) {
+        this.sentenceDao = sentenceDao;
+    }
 }
