9815efa747004a8d90fd74ae643a48f37658495c
heroinedor <heroinedor@gmail.com>
2017-07-21 17:53:17 +0200

    Etape 2 OK : simple job that opens a file and copy content and title to database

--- launchJob.bat
+++ launchJob.bat
@@ -1,1 +1,1 @@
-java -cp "target/*" org.springframework.batch.core.launch.support.CommandLineJobRunner context.xml helloWorldJob
+java -cp "target/*" org.springframework.batch.core.launch.support.CommandLineJobRunner context.xml biblioBatchJob
