6b1efe83af88232af9c3c784b8f25137a9d9f8f0
heroinedor <heroinedor@gmail.com>
2017-07-26 09:19:45 +0200

    TODO for the jobParameters
    correction on TODO message
    
    Corrections on TODO and modification on pom.xml

--- src/test/java/biblio/book/BiblioBatchJobITCase.java
+++ src/test/java/biblio/book/BiblioBatchJobITCase.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -21,7 +23,9 @@ public class BiblioBatchJobITCase {
 
     @Test
     public void testJob() throws Exception {
-
+        //TODO add 2 jobParameters to the jobLaunch method :
+        // -  key : "fileName", value "file:C:/temp/books/lorem-ipsum.txt" => to set the file to load in DB
+        // -  key : "random", value : a random number between 0 and 1000000 => a random parameter to let the job be restarted as much as wanted
         JobExecution jobExecution = jobLauncherTestUtils.launchJob();
 
         Assert.assertEquals("COMPLETED", jobExecution.getExitStatus().getExitCode());
